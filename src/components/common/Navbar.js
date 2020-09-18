e788c1e973c901442e08f151653a9334c89430b2
puja <gc.puja1@yahoo.com>
2020-09-18 12:05:42 +0100

    added dropdown in the navbar

--- src/components/common/Navbar.js
+++ src/components/common/Navbar.js
@@ -1,21 +1,70 @@
-import React, { Fragment } from 'react'
 
-const Navbar = () => {
-  return (
-    <Fragment>
-      <div className="grid-container">
-        <div className="webname">webname</div>
-        <div className="menuBtn recipes"><button>recipes</button></div>
-        <div className="menuBtn types" ><button>types</button></div>
-      </div>  
-    </Fragment>
-  )
+import React, { Component, Fragment } from 'react'
+import { Link } from 'react-router-dom'
+
+
+class Navbar extends Component {
+
+
+  state = {
+    dropCuisines: false,
+    dropTypes: false
+  }
+
+  
+  handleDropDown = (e) => {
+    if (e === 'cuisines') {
+      this.setState({ dropTypes: false })
+      return this.setState({ dropCuisines: !this.state.dropCuisines })
+    }
+    if (e === 'types') {
+      this.setState({ dropCuisines: false })
+      return this.setState({ dropTypes: !this.state.dropTypes })
+    }
+  }
+
+  render() {
+    return (
+
+      <Fragment>
+        <div className="front-container">
+          <div className="grid-container">
+            <div className="webname"><Link to='/'>webname</Link></div>
+
+            <div className="buttonCuisinesSize" style={{position: 'relative'}}>
+              <div className="menuBtn choose cuisines"><button onClick={e=>this.handleDropDown('chooseCuisines')}>choose cuisines</button></div>
+              {
+                this.state.dropCuisines && <Fragment>
+                  <div className="navi">
+                      empty
+                  </div>
+                </Fragment>
+              }
+            </div>
+
+
+
+            <div className="buttonTypesSize" style={{position: 'relative'}}>
+              <div className="menuBtn types" ><button onClick={e=>this.handleDropDown('types')}>Meal Types</button></div>
+              {
+                this.state.dropTypes && <Fragment>
+                  <div className="navi2">
+                    <div className="nav"><Link to="/breakfast">breakfast</Link></div>
+                    <div className="nav"><Link to="/brunch"> brunch </Link></div>
+                    <div className="nav"><Link to="/lunch"> Lunch </Link></div>
+                    <div className="nav"><Link to="/dinner"> Dinner </Link></div>
+                  </div>
+                </Fragment>
+              }
+
+            </div>
+
+          </div>
+
+        </div>
+      </Fragment>
+    )
+  }
 }
 export default Navbar
-
-
-
-
-
-
 
